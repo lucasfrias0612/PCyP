c88a7204344b7edbbfe6bae089805b9bb3a9cab0
root <root@LucasUNPAZ.unpaz.edu.ar>
2019-04-01 18:04:53 -0300

    Comienzo de tarea numero 3 Multiplicador de matrices

---
+++ MultiplicacionMatrices/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using LucasLib;
+
+namespace MultiplicacionMatrices {
+
+    class Program:ConsoleProgram {
+
+        public static void Main () {
+            Program program = new Program ();
+            program.run ();
+            program.pause ();
+        }
+
+        override
+        public void run () {
+        
+        }
+    }
+
+    class Matrix{
+
+        private int width;
+        private int heigth;
+
+        public Matrix(int width, int heigth){
+            this.width=width;
+            this.heigth=heigth;
+        }
+    }
+}
