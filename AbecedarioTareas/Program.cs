5168920a33d680615ea58c08416383011577d231
LucasFrias94 <lucasmatiasfrias@live.com>
2019-04-10 00:14:02 -0300

    Se agrego el ejercicio que consistia en generar dos tareas que vayan escribiendo las letras del abecedario una a la vez con variables compartidas para sincrinizarse.

---
+++ AbecedarioTareas/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using LucasLib;
+
+namespace AbecedarioTareas {
+    class Program : ConsoleProgram {
+        static void Main (string[] args) {
+            Program p = new Program ();
+            p.run ();
+        }
+        override
+        public void run () {
+            Dictionary<char, string> alphabet = new Dictionary<char, string> ();
+            int index = 0;
+            const int indexLimit = 26;
+            const int startChar = 65;
+            Task t1 = new Task (() => {
+                while (index < indexLimit) {
+                    if (isPar (index)) {
+                        alphabet.Add ((char) (index + startChar), "Tarea 1");
+                        index++;
+                    }
+                }
+            });
+
+            Task t2 = new Task (() => {
+                while (index < indexLimit) {
+                    if (!isPar (index)) {
+                        alphabet.Add ((char) (index + startChar), "Tarea 2");
+                        index++;
+                    }
+                }
+            });
+
+            Task t3 = new Task (() => {
+                foreach (KeyValuePair<char, string> pair in alphabet) {
+                    showMsg ("Letra {" + pair.Key + "} Escrita por {" + pair.Value + "}");
+                }
+                pause ();
+            });
+
+            t1.Start ();
+            t2.Start ();
+
+            Task.WaitAny (new Task[] { t1, t2 });
+
+            t3.Start ();
+            t3.Wait ();
+
+        }
+
+        static bool isPar (int n) {
+            return n % 2 == 0;
+        }
+    }
+}
