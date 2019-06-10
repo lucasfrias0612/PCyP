ca424fb58cc6b86d1c4291768ac0690044ac87ff
root <root@LucasUNPAZ.unpaz.edu.ar>
2019-06-10 17:18:21 -0300

    Se implemento un ejemplo del patrón Wait All One by One

---
+++ WaitAllOneByOne/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+using LucasLib;
+
+namespace WaitAllOneByOne {
+    class Program : ConsoleProgram {
+        static void Main (string[] args) {
+            Program program = new Program ();
+            program.run ();
+            program.pause ();
+        }
+
+        override
+        public void run () {
+            List<Task<string>> tasks = new List<Task<string>> ();
+            for (int i=0;i<10;i++) {
+                tasks.Add(new Task<string>((index) => {
+                    Thread.SpinWait (10000000);
+                    return "Tarea "+index+" finalizada.";
+                }, i));
+            }
+            foreach(Task t in tasks){
+                t.Start();
+            }
+            while(tasks.Count>0){
+                int index = Task.WaitAny(tasks.ToArray());
+                showMsg("Result: "+tasks.ToArray()[index].Result);
+                tasks.RemoveAt(index);
+            }
+            showMsg ("Hilo principal esperó a las tareas una por una");
+        }
+    }
+
+}
