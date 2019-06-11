e4e65a741266965d8a8126460a3d7ddcc09773ce
root <root@LucasUNPAZ.unpaz.edu.ar>
2019-06-11 17:52:48 -0300

    Ejercicio Netflix terminado

--- Netflix/Program.cs
+++ Netflix/Program.cs
@@ -1,112 +1,218 @@
-﻿using System;
+﻿/* Program.cs*/
+
+//
+// Parallel C# Version using MapReduce-like approach  [ End result?  Fast, near-linear speedup ]
+//
+// In parallel, processes a text file of movie reviews, one per line, 
+// the format of which are:
+//
+//   movie id, user id, rating (1..5), date (YYYY-MM-DD)
+//
+// The output are the top 10 users who reviewed the most movies, in 
+// descending order (so the user with the most reviews is listed 
+// first).
+//
+
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.IO;
-using System.Linq; // Para el OrderByDescending method
-using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+using System.Threading;
 using System.Threading.Tasks;
-using LucasLib;
 
-namespace Netflix {
-    class Program : ConsoleProgram {
-        static void Main (string[] args) {
-            Program program = new Program ();
-            program.run ();
-            program.pause ();
-        }
-        override
-        public void run () {
-            Stopwatch s = new Stopwatch ();
-            s.Start ();
-            QualificationRatingData data = new QualificationRatingData ();
 
-            showMsg ("Diccionario:\n");
-            foreach (var item in data.getUserQualifications ().OrderByDescending (pair => pair.Value).Take (10)) {
-                showMsg ("User {" + item.Key + "} Qualifications {" + item.Value + "}");
+namespace Test
+{
+	class Program
+	{
+
+		static void Main(string[] args)
+		{
+			var sw = System.Diagnostics.Stopwatch.StartNew();
+
+			//
+			// Process cmd-line args, welcome msg:
+			//
+			string infile;
+
+			ProcessCmdLineArgs(args, out infile);
+
+			// 
+			// Foreach record, parse and aggregate the pairs <userid, num reviews>:
+			//
+			sw.Restart();
+
+			Dictionary<int, int> ReviewsByUser = new Dictionary<int, int>();
+
+			//
+			//foreach (string line in File.ReadLines(infile))
+			//
+			Parallel.ForEach(File.ReadLines(infile),
+
+				//
+				// Initializer:  create task-local Dictionary:
+				//
+				() => { return new Dictionary<int, int>(); },
+
+
+				//
+				// Loop-body: work with TLS which represents a local Dictionary,
+				// mapping our results into this local dictionary:
+				//
+				(line, loopControl, localD) =>
+				{
+					//
+					// movie id, user id, rating (1..5), date (YYYY-MM-DD)
+					//
+					int userid = parse(line);
+
+					if (!localD.ContainsKey(userid))  // first review:
+						localD.Add(userid, 1);
+					else  // another review by same user:
+						localD[userid]++;
+
+					return localD;  // return out so it can be passed back in later:
+				},
+
+				//
+				// Finalizer: reduce individual local dictionaries into global dictionary:
+				//
+				(localD) =>
+				{
+					lock (ReviewsByUser)
+					{
+						//
+						// merge into global data structure:
+						//
+						foreach (int userid in localD.Keys)
+						{
+							int numreviews = localD[userid];
+
+							if (!ReviewsByUser.ContainsKey(userid))  // first review:
+								ReviewsByUser.Add(userid, numreviews);
+							else  // another review by same user:
+								ReviewsByUser[userid] += numreviews;
+						}
+					}
+				}
+
+			);
+
+			//
+			// Sort pairs by num reviews, descending order, and take top 10:
+			//
+			var sort = from user in ReviewsByUser
+								 orderby user.Value descending, user.Key ascending
+								 select new { Userid = user.Key, NumReviews = user.Value };
+
+			var top10 = sort.Take(10).ToList();
+
+			long timems = sw.ElapsedMilliseconds;
+
+			//
+			// Write out the results:
+			//
+			Console.WriteLine();
+			Console.WriteLine("** Top 10 users reviewing movies:");
+
+			foreach (var user in top10)
+				Console.WriteLine("{0}: {1}", user.Userid, user.NumReviews);
+
+			// 
+			// Done:
+			//
+			double time = timems / 1000.0;  // convert milliseconds to secs
+
+			Console.WriteLine();
+			Console.WriteLine("** Done! Time: {0:0.000} secs", time);
+			Console.WriteLine();
+			Console.WriteLine();
+			Console.WriteLine();
+			
+			Console.Write("Press a key to exit...");
+			Console.ReadKey();
+		}
+
+
+		/// <summary>
+		/// Parses one line of the netflix data file, and returns the userid who reviewed the movie.
+		/// </summary>
+		/// <param name="line"></param>
+		/// <returns></returns>
+		private static int parse(string line)
+		{
+			char[] separators = { ',' };
+
+			string[] tokens = line.Split(separators);
+
+			int movieid = Convert.ToInt32(tokens[0]);
+			int userid = Convert.ToInt32(tokens[1]);
+			int rating = Convert.ToInt32(tokens[2]);
+			DateTime date = Convert.ToDateTime(tokens[3]);
+
+			return userid;
+		}
+
+
+		/// <summary>
+		/// Processes any command-line args (currently there are none), returning
+		/// the input file to read from.
+		/// </summary>
+		/// <param name="args">cmd-line args to program</param>
+		/// <param name="infile">input file to read from</param>
+		///
+		static void ProcessCmdLineArgs(string[] args, out string infile)
+		{
+			String version, platform;
+
+#if DEBUG
+			version = "debug";
+#else
+			version = "release";
+#endif
+
+#if _WIN64
+	platform = "64-bit";
+#elif _WIN32
+	platform = "32-bit";
+#else
+			platform = "any-cpu";
+#endif
+
+			//
+			// Defaults:
+			//
+			infile = "ratings.txt";
+
+            //
+            // There should be no command-line args:
+            //
+            if (args.Length != 0)
+            {
+                infile = args[0];
             }
-            s.Stop ();
-            showMsg ("Tiempo de ejecución: " + s.ElapsedMilliseconds + " miliseg.");
-        }
-    }
 
-    class QualificationRatingData {
-        private static readonly string DEFAULT_PATH = "ratings.txt";
-        private Dictionary<int, int> userQualifications;
-        public QualificationRatingData () {
-            StreamReader objReader = new StreamReader (DEFAULT_PATH);
-            string s = objReader.ReadLine ();
-            List<string> lines= new List<string>();
-            while (s!=null){
-                lines.Add(s);
-                s = objReader.ReadLine ();
-            }
-            userQualifications = new Dictionary<int, int> ();
-            Parallel.ForEach (
-                lines, //Datasource
-                () => 0, //Initilizer
-                (line, loopState, subtotal) => //Task body
-                {
-                    Qualification qualification = new Qualification (line);
-                    addUserQualification (qualification.getUserId ());
-                    return subtotal.Remove((string)line);
-                },
-                (subtotal) => //Finalizer
-                {
-                    //Interlocked.Add (ref count, subtotal);
-                }
-            );
-            objReader.Close ();
-        }
+            if (!File.Exists(infile))
+			{
+				Console.WriteLine("** Error: infile '{0}' does not exist.", infile);
+				Console.WriteLine();
+				Console.WriteLine();
+				System.Environment.Exit(-1);
+			}
 
-        private void addUserQualification (int userId) {
-            if (userQualifications.ContainsKey (userId))
-                userQualifications[userId]++;
-            else
-                userQualifications.Add (userId, 1);
-        }
+			//
+			// Process command-line args to get infile and outfile:
+			//
+			FileInfo fi = new FileInfo(infile);
+			double sizeinMB = fi.Length / 1048576.0;
 
-        public Dictionary<int, int> getUserQualifications () {
-            return this.userQualifications;
-        }
-    }
+			Console.WriteLine("** Parallel, MapReduce Top-10 Netflix Data Mining App [{0}, {1}] **", platform, version);
+			Console.Write("   Infile:  '{0}' ({1:#,##0.00 MB})", infile, sizeinMB);
+			Console.WriteLine();
+		}
 
-    class Qualification {
-        private int movieId;
-        private int userId;
-        private int qualification;
-        private DateTime qualificationDate;
-        public Qualification (int movieId, int userId, int qualification, DateTime qualificationDate) {
-
-        }
-
-        public Qualification (string s) {
-            string[] line = s.Split (",");
-            this.movieId = Int32.Parse (line[0]);
-            this.userId = Int32.Parse (line[1]);
-            this.qualification = Int32.Parse (line[2]);
-            this.qualificationDate = parseToDate (line[3]);
-        }
-
-        private DateTime parseToDate (string s) {
-            string[] line = s.Split ("-");
-            return new DateTime (Int32.Parse (line[0]), Int32.Parse (line[1]), Int32.Parse (line[2]));
-        }
-
-        override
-        public string ToString () {
-            return "MovieId: " + movieId + "\tUserId: " + userId + "\tQualification: " + qualification + "\tDate: " + qualificationDate.ToString () + "\n";
-        }
-
-        public int getUserId () {
-            return this.userId;
-        }
-        public int getMovieId () {
-            return this.movieId;
-        }
-        public int getQualification () {
-            return this.qualification;
-        }
-        public DateTime GetDate () {
-            return this.qualificationDate;
-        }
-    }
-}
+	}//class
+}//namespace
---
+++ Test/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Parcial2 {
+    class Program {
+        static void Main (string[] args) {
+            var sw = System.Diagnostics.Stopwatch.StartNew ();
+            List<Record> records = new List<Record> ();
+            Dictionary<string, int> LengthOfRoutes = new Dictionary<string, string> ();
+            String line;
+            try {
+                StreamReader sr = new StreamReader ("rutas_nacionales.csv");
+                line = sr.ReadLine ();
+                while (line != null) {
+                    records.Add (new Record (line));
+                    line = sr.ReadLine ();
+                }
+                sr.Close ();
+            } catch (Exception e) {
+                Console.WriteLine ("Exception: " + e.Message);
+            }
+
+            Parallel.ForEach (records,
+                () => { return new Dictionary<string, int> (); },
+
+                (r, loopControl, localD) => {
+                    int length = Convert.ToInt32 (r.lengthKm);
+                    if (!localD.ContainsKey (r.province)) // first review:
+                        localD.Add (r.province, length);
+                    else // another review by same user:
+                        localD[r.province] += length;
+                    return localD; // return out so it can be passed back in later:
+                },
+                //
+                // Finalizer: reduce individual local dictionaries into global dictionary:
+                //
+                (localD) => {
+                    lock (LengthOfRoutes) {
+                        foreach (string province in localD.Keys) {
+                            int numreviews = localD[province];
+                            if (!LengthOfRoutes.ContainsKey (province)) // first review:
+                                LengthOfRoutes.Add (province, numreviews);
+                            else // another review by same user:
+                                LengthOfRoutes[province] += numreviews;
+                        }
+                    }
+                }
+            );
+            long timems = sw.ElapsedMilliseconds;
+            //
+            // Write out the results:
+            //
+            Console.WriteLine ();
+            Console.WriteLine ("** Resultado:");
+
+            foreach (var record in LengthOfRoutes)
+                Console.WriteLine ("{0}: {1}", record.Key, record.Value);
+
+            // 
+            // Done:
+            //
+            double time = timems / 1000.0; // convert milliseconds to secs
+            Console.WriteLine ();
+            Console.WriteLine ("** Done! Time: {0:0.000} secs", time);
+            Console.WriteLine ();
+            Console.WriteLine ();
+            Console.WriteLine ();
+
+            Console.Write ("Press a key to exit...");
+            Console.ReadKey ();
+        }
+    }
+
+    class Record {
+        public string province;
+        public string responsible;
+        public string route;
+        public string type;
+        public string lengthKm;
+
+        public Record (string fileLine) {
+            string[] tokens = fileLine.Split (",");
+            this.province = tokens[0];
+            this.responsible = tokens[1];
+            this.route = tokens[2];
+            this.type = tokens[3];
+            this.lengthKm = tokens[4];
+        }
+    }
+}
