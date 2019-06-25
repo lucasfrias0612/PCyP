9258f92ed8420d91d80cdb4a6ee2931ba2b29f71
root <root@LucasUNPAZ.unpaz.edu.ar>
2019-06-25 17:56:06 -0300

    Test agregado

---
+++ test/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test {
+    class Program {
+
+        private static string infile = "vial.csv";
+
+        static void Main (string[] args) {
+            var sw = System.Diagnostics.Stopwatch.StartNew ();
+            if (File.Exists (infile)) {
+                string[] lines = File.ReadLines (infile).ToArray ();
+
+                Dictionary<string, Dictionary<string, int>> finalDic = new Dictionary<string, Dictionary<string, int>> ();
+
+                Parallel.ForEach (lines,
+
+                    () => { return new Dictionary<string, Dictionary<string, int>> (); },
+
+                    (line, loopControl, localD) => {
+                        if (Record.isValid (line)) {
+                            Record r = new Record (line);
+                            if (!localD.ContainsKey (r.card)) {
+                                Dictionary<string, int> d = new Dictionary<string, int> ();
+                                d.Add (r.route, r.routeLength);
+                                localD.Add (r.card, d);
+                            } else {
+                                if (!localD[r.card].ContainsKey (r.route))
+                                    localD[r.card].Add (r.route, r.routeLength);
+                                else
+                                    localD[r.card][r.route] += r.routeLength;
+                            }
+                        }
+                        return localD;
+                    },
+
+                    (localD) => {
+                        lock (finalDic) {
+                            foreach (string card in localD.Keys) {
+                                if (!finalDic.ContainsKey (card))
+                                    finalDic.Add (card, localD[card]);
+                                else {
+                                    Dictionary<string, int>[] dics = { finalDic[card], localD[card] };
+                                    finalDic[card] = Dictionary<string, int>.Merge (dics);
+                                }
+                            }
+                        }
+                    }
+
+                );
+
+                double time = sw.ElapsedMilliseconds / 1000.0; // convert milliseconds to secs
+
+                Console.WriteLine ();
+                Console.WriteLine ("** Done! Time: {0:0.000} secs", time);
+                Console.WriteLine ();
+                Console.Write ("Press a key to exit...");
+                Console.ReadKey ();
+            } else {
+                Console.WriteLine ("** Error: infile '{0}' does not exist.", infile);
+                Console.WriteLine ();
+                System.Environment.Exit (-1);
+            }
+        }
+    } //class
+
+    class Record {
+        static char CSV_DELIMITER = ',';
+
+        public string card;
+        public string route;
+        public int routeLength;
+
+        public Record (string input) {
+            string[] tokens = input.Split (CSV_DELIMITER);
+            if (tokens.Length == 5) {
+                card = tokens[0];
+                route = tokens[3];
+                Int32.TryParse (tokens[4], out routeLength);
+            }
+        }
+
+        public static bool isValid (string l) {
+            bool b = false;
+            string[] tokens = l.Split (CSV_DELIMITER);
+            if (tokens.Length == 5) {
+                var converted = 0;
+                bool success = Int32.TryParse (tokens[4], out converted);
+                b = !string.IsNullOrEmpty (tokens[0]) && !string.IsNullOrEmpty (tokens[3]) && success;
+            }
+            return b;
+        }
+    }
+}
---
+++ test/Program2.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<int, double> longitudesPorCarta = new Dictionary<int, double>();
+
+            List<string> archivos = new List<string>();
+            for (int iNroArchivo = 1; iNroArchivo <= 72; iNroArchivo++)
+            {
+                archivos.Add(@"data\vial" + iNroArchivo.ToString().PadLeft(2, '0') + ".csv");
+            }
+
+            //
+            //foreach (string line in File.ReadLines(infile))
+            //
+            Parallel.ForEach(archivos,
+
+                //
+                // Initializer:  create task-local Dictionary:
+                //
+                () => { return new Dictionary<int, double>(); },
+
+
+                //
+                // Loop-body: work with TLS which represents a local Dictionary,
+                // mapping our results into this local dictionary:
+                //
+                (archivo, loopControl, localD) =>
+                {
+
+                    foreach (string line in File.ReadLines(archivo))
+                    {
+                        char[] separators = { ',' };
+                        string[] tokens = line.Split(separators);
+                        int carta = Convert.ToInt32(tokens[0]);
+                        double longitud;
+                        if (CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator == ",")
+                        {
+                            longitud = double.Parse(tokens[4].Replace(".", ","));
+                        }
+                        else
+                        {
+                            longitud = double.Parse(tokens[4]);
+                        }
+
+                        if (!localD.ContainsKey(carta))  // primera vez que llega esta carta:
+                            localD.Add(carta, 1);
+                        else  // otra longitud de la misma carta:
+                            localD[carta] += longitud;
+                    }
+
+                    return localD;  // return out so it can be passed back in later:
+                },
+
+                //
+                // Finalizer: reduce individual local dictionaries into global dictionary:
+                //
+                (localD) =>
+                {
+                    lock (longitudesPorCarta)
+                    {
+                        //
+                        // merge into global data structure:
+                        //
+                        foreach (int carta in localD.Keys)
+                        {
+                            double longitud = localD[carta];
+
+                            if (!longitudesPorCarta.ContainsKey(carta))  // first review:
+                                longitudesPorCarta.Add(carta, longitud);
+                            else  // another review by same user:
+                                longitudesPorCarta[carta] += longitud;
+                        }
+                    }
+                }
+
+            );
+
+            //
+            // Write out the results:
+            //
+            Console.WriteLine();
+            Console.WriteLine("** Longitudes de caminos por carta");
+
+            double total = 0;
+            foreach (int carta in longitudesPorCarta.Keys)
+            {
+                double longitud = longitudesPorCarta[carta];
+                total += longitud;
+                Console.WriteLine("Carta {0}: {1} m.", carta, longitud);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("** Longitud total del país: {0}", total);
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+    }
+}
